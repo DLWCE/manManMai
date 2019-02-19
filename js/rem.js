aa2cb15bbc926af1e8cec4c9d6b1d105776e8f88
DLWCE <891508062@qq.com>
2019-02-20 01:05:08 +0800

    夏庆龙慢慢买项目3.0版本

---
+++ js/rem.js
@@ -0,0 +1,25 @@
+ //默认调用一次设置
+ setHtmlFontSize();
+
+ function setHtmlFontSize() {
+     // 1. 获取当前屏幕的宽度
+     var windowWidth = document.documentElement.offsetWidth;
+     // console.log(windowWidth);
+     // 2. 定义标准屏幕宽度 假设375
+     var standardWidth = 375;
+     // 3. 定义标准屏幕的根元素字体大小 假设100px  
+     var standardFontSize = 100;
+     // 4. 计算当前屏幕对应的根元素字体大小
+     var nowFontSize = windowWidth / standardWidth * standardFontSize;
+     // 5. 定义根元素字体大小最大为200px,进行判断
+     var maxFontSize = 200;
+        if(nowFontSize > maxFontSize){
+            nowFontSize = maxFontSize;
+        }
+     // console.log(nowFontSize);
+     // 5. 把当前计算的根元素的字体大小设置到html上
+     document.querySelector('html').style.fontSize = nowFontSize +'px';
+         
+ }
+ // 6. 添加一个屏幕宽度变化的事件  屏幕变化就触发变化根元素字体大小计算的js 
+ window.addEventListener('resize', setHtmlFontSize);
